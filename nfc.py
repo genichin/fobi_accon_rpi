e18fc08e677f2afa278f2fa640967d756acf8bed
Justin Ko <genichin@gmail.com>
2021-11-01 03:45:00 +0000

    NFC 데이터 획득 방법 변경

--- nfc.py
+++ nfc.py
@@ -1,17 +1,23 @@
-import time
-from pynfc import Nfc, Desfire, Timeout
+import sys
+import queue
+import subprocess
 import fba_common
 
-def nfcscan_start(msg_q) :    
+def nfcscan_start(msg_q) :
     while True :
-        try :
-            n = Nfc("pn532_i2c:/dev/i2c-1")    
-            for target in n.poll():
-                #print(target)
-                print(target.uid)
-                msg_q.put((fba_common.EVENT_NFC_DETECTED, target.uid))
-                time.sleep(3)
-        except Exception as e :
-            print(e)
-            pass
-        
+        p = subprocess.Popen(['stdbuf', '-o0', 'nfc-poll'],
+                        stdout=subprocess.PIPE,
+                        stderr=subprocess.STDOUT,
+                        universal_newlines=True)
+                        
+        while p.poll() == None:
+            out = p.stdout.readline()
+            if out.find('UID (') != -1 :
+                uid = out[out.find('): ')+3:].replace("\n", "")
+                uid = uid.replace(" ", "")
+                msg_q.put((fba_common.EVENT_NFC_DETECTED, uid))
+                print(uid)
+
+if __name__ == '__main__':
+    message_queue = queue.Queue()
+    nfcscan_start(message_queue)
